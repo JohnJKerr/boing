a01dd0c197fe3c9dcaa97f1ea850a798200cda6c
John Kerr <kerr.john@gmail.com>
2020-02-26 00:03:13 +0000

    Added ball speed-up, added bat movement

--- Ball.cs
+++ Ball.cs
@@ -3,6 +3,8 @@ using System;
 
 public class Ball : KinematicBody2D
 {
+    private const int MaxSpeed = 50;
+    private int _speed = 10;
     private Vector2 _motion = Vector2.Zero;
 
     public override void _Ready()
@@ -14,8 +16,14 @@ public class Ball : KinematicBody2D
     public override void _Process(float delta)
     {
         base._Process(delta);
-        var collisionInfo = MoveAndCollide(_motion);
-        if (collisionInfo != default)
-            _motion = _motion.Bounce(collisionInfo.Normal);
+        Move();
+    }
+
+    private void Move()
+    {
+        var collisionInfo = MoveAndCollide(_motion * _speed);
+        if (collisionInfo == default) return;
+        if(_speed < MaxSpeed) _speed += 5;
+        _motion = _motion.Bounce(collisionInfo.Normal);
     }
 }
--- Bat.cs
+++ Bat.cs
@@ -3,13 +3,29 @@ using System;
 
 public class Bat : KinematicBody2D
 {
+	private const int Speed = 200;
 	[Export] private int _playerNumber = 0;
+	private Vector2 _motion = Vector2.Zero;
 
 	private Sprite Sprite => GetNode<Sprite>(nameof(Sprite));
 
 	public override void _Ready()
 	{
 		SetTexture();
+	}
+
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+		Move();
+	}
+
+	private void Move()
+	{
+		_motion.y = 0;
+		if (Input.IsActionPressed("up")) _motion.y -= Speed;
+		if (Input.IsActionPressed("down")) _motion.y += Speed;
+		MoveAndSlide(_motion);
 	}
 
 	private void SetTexture(int number = 0)
